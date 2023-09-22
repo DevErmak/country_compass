61e63a21a715db372ffbdc5cba2ec87842791953
DevErmak <o.khasanov.dev@gmail.com>
2023-09-22 18:22:24 +0300

    save data

---
+++ custom.d.ts
@@ -0,0 +1,19 @@
+declare module '*.svg' {
+  const content: any;
+  // const content: string;
+  // const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+// declare module '*.svg' {
+//   import type * as React from 'react';
+
+//   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+//   export default ReactComponent;
+// }
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
---
+++ src/components/Home/HomeContainer.tsx
@@ -0,0 +1,13 @@
+// import { ReactComponent as Logo } from '../../static/images/logo.svg';
+import Logo from '../../static/images/logo.svg';
+
+type Props = {};
+
+export default function HomeContainer({}: Props) {
+  return (
+    <div>
+      {/* <Logo width={20} height={20} /> */}
+      <Logo />
+    </div>
+  );
+}
